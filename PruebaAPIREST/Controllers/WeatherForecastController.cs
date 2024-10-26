b7121b2c666df93688f701961a41c8c9b6798151
Felipe <60165757+felipe-feijoo@users.noreply.github.com>
2024-10-26 14:49:43 -0300

    Update WeatherForecastController.cs

--- PruebaAPIREST/Controllers/WeatherForecastController.cs
+++ PruebaAPIREST/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace PruebaAPIREST.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "history2")]
+        [HttpGet(Name = "history22")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
